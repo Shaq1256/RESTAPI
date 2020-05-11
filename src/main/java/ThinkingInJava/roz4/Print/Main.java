0947d80f89fccc2400a49dfa7cb40d8cd3946ddd
Shaq1256 <lukaszmioduszewskii@gmail.com>
2020-05-11 12:00:28 +0100

    Thinking in Java (metoda Print)

---
+++ src/main/java/ThinkingInJava/roz4/Print/Main.java
@@ -0,0 +1,11 @@
+package ThinkingInJava.roz4.Print;
+
+import static ThinkingInJava.roz4.Print.Print.*;
+
+public class Main {
+    public static void main(String[] args) {
+        print("Tylko u Nas!!!");
+        print(100);
+        print("Wygrana " + 100 + "%");
+    }
+}
